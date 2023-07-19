9d033068c6216f45acc0b9623474a6aa5805a4f4
João <jlcarruda3@gmail.com>
2023-07-19 02:34:12 -0300

    chore: initial group labels + ascii art on giga theme

--- dotfiles/.config/qtile/functions.py
+++ dotfiles/.config/qtile/functions.py
@@ -684,11 +684,11 @@ groups = []
 group_names = ["Escape","1","2","3","4","5","6","7","8","9"]
 
 #### Groups Labels
-group_labels=["零","一","二","三","四","五","六","七","八","九"] # Kanji Numbers
+#group_labels=["零","一","二","三","四","五","六","七","八","九"] # Kanji Numbers
 #group_labels=["0","1","2","3","4","5","6","7","8","9"] # Numbers
 #group_labels=["","","","","","","","","",""] # Circles
 #group_labels=["","","","","","","","","",""] # Dot Circles
-#group_labels=["","","","","","","","","",""] # Custom
+group_labels=["","","","","","","","","",""] # Custom
 #group_labels=["","","","","","","","","",""] # Star Wars
 
 
